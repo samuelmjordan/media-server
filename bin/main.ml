8d48e7aad95fa4d576d31763d606c572f67e373d
jordan12sam <jordan12sam@gmail.com>
2025-07-19 16:09:17 +0100

    lwx

--- bin/main.ml
+++ bin/main.ml
@@ -1,3 +1,23 @@
+let count = ref 0
+
+let count_requests inner_handler request =
+  count := !count + 1;
+  inner_handler request
+
 let () =
-  Dream.run (fun _ ->
-    Dream.html "Good morning, world!")
+  Dream.run 
+    @@ Dream.logger 
+    @@ count_requests
+    @@ Dream.router
+      [
+
+        Dream.get "/" 
+          (fun _ -> Dream.html "Good morning, world!");
+
+        Dream.post "/echo" 
+          (fun request -> let%lwt body = Dream.body request in Dream.respond ~headers:["Content-Type", "application/octet-stream"] body);
+
+        Dream.get "/count" 
+          (fun _ -> Dream.html (Printf.sprintf "Saw %i request(s)!" !count));
+
+      ]
