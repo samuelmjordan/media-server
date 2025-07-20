5e03fb8875bd510e4b43b9d1c9dcca81443bfcf2
jordan12sam <jordan12sam@gmail.com>
2025-07-20 11:22:31 +0100

    redundant conversions

--- lib/database/repository/User_Repository.ml
+++ lib/database/repository/User_Repository.ml
@@ -1,10 +1,5 @@
 open Lwt.Syntax
 
-let convert_row_to_user (user_id_str, name, email) =
-  match User.User_Uuid.from_string user_id_str with
-    | Ok user_id -> Ok { User.user_id; name; email }
-    | Error _ -> Error "invalid user_id in db"
-
 let create ~user_id ~name ~email =
   Db.with_connection (fun (module Db : Caqti_lwt.CONNECTION) ->
     Db.exec User_Statements.Q.create_user (user_id, name, email))
@@ -13,10 +8,8 @@ let find_by_id ~user_id =
   let* result = Db.with_connection (fun (module Db : Caqti_lwt.CONNECTION) ->
     Db.find_opt User_Statements.Q.get_user_by_id user_id) in
   match result with
-  | Ok (Some row) -> 
-    (match convert_row_to_user row with
-    | Ok user -> Lwt.return (Ok (Some user))
-    | Error e -> Lwt.return (Error e))
+  | Ok (Some (user_id, name, email)) -> 
+    Lwt.return (Ok (Some { User.user_id; name; email }))
   | Ok None -> Lwt.return (Ok None)
   | Error e -> Lwt.return (Error (Caqti_error.show e))
 
@@ -25,12 +18,7 @@ let find_all () =
     Db.collect_list User_Statements.Q.get_all_users ()) in
  match result with
   | Ok rows ->
-    let rec convert_users acc = function
-      | [] -> Ok (List.rev acc)
-      | row :: rest ->
-        (match convert_row_to_user row with
-        | Ok user -> convert_users (user :: acc) rest
-        | Error e -> Error e)
-    in
-    Lwt.return (convert_users [] rows)
+    let users = List.map (fun (user_id, name, email) -> 
+      { User.user_id; name; email }) rows in
+    Lwt.return (Ok users)
   | Error _ -> Lwt.return (Error "db error")
