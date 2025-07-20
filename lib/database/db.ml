07fc6b2c58f7a706902cb7e581b0523eb642093f
jordan12sam <jordan12sam@gmail.com>
2025-07-20 10:03:23 +0100

    uid caqti type

--- lib/database/db.ml
+++ lib/database/db.ml
@@ -4,17 +4,17 @@ module Q = struct
   open Caqti_request.Infix
   
   let create_user =
-    Caqti_type.(t3 user_uuid_type string string) ->. Caqti_type.unit
+    Caqti_type.(t3 User.User_Uuid.caqti_type string string) ->. Caqti_type.unit
     @@
     "INSERT INTO user_ (user_id, name, email) VALUES (?, ?, ?)"
 
   let get_all_users =
-    Caqti_type.unit ->* Caqti_type.(t3 user_uuid_type string string)
+    Caqti_type.unit ->* Caqti_type.(t3 User.User_Uuid.caqti_type string string)
     @@
     "SELECT user_id, name, email FROM user_ ORDER BY created_at"
 
   let get_user_by_id =
-    user_uuid_type ->? Caqti_type.(t3 user_uuid_type string string)
+    User.User_Uuid.caqti_type ->? Caqti_type.(t3 User.User_Uuid.caqti_type string string)
     @@
     "SELECT user_id, name, email FROM user_ WHERE user_id = ?"
 end
