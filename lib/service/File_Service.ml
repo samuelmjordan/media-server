34cea31c43f5520946e3a9826a262d0e2d4c1f27
jordan12sam <jordan12sam@gmail.com>
2025-07-20 14:20:36 +0100

    recursive directories

--- lib/service/File_Service.ml
+++ lib/service/File_Service.ml
@@ -1,5 +1,6 @@
 open Lwt.Syntax
-let read_dir path =
+
+let rec read_dir path =
   let* handle = Lwt_unix.opendir path in
   let rec loop acc =
     try%lwt
@@ -7,14 +8,21 @@ let read_dir path =
       if entry = "." || entry = ".." then
         loop acc
       else
-        let* stats = Lwt_unix.stat (Filename.concat path entry) in
+        let full_path = Filename.concat path entry in
+        let* stats = Lwt_unix.stat full_path in
         let is_directory = stats.st_kind = S_DIR in
         let size_bytes = stats.st_size in
         let file = File.make ~path ~name:entry ~is_directory ~size_bytes in
-        loop (file :: acc)
+        let acc = file :: acc in
+        if is_directory then
+          let* sub_files = read_dir full_path in
+          loop (sub_files @ acc)
+        else
+          loop acc
     with 
       | End_of_file ->
         let* () = Lwt_unix.closedir handle in
         Lwt.return (List.rev acc)
       | _ -> loop acc
-    in loop []
+  in 
+  loop []
