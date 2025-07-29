467caa4d8ce6a81e1f621671ca3cc04be9bca439
jordan12sam <jordan12sam@gmail.com>
2025-07-29 17:34:45 +0100

    remove user fluff

--- test/fixtures/Runner.ml
+++ test/fixtures/Runner.ml
@@ -4,8 +4,6 @@ let () =
   Config.initialize_configs ();
   Lwt_main.run @@ 
   Alcotest_lwt.run "Tests" [
-    User_Handler_Test.cases;
     File_Handler_Test.cases;
     File_Repository_Test.cases;
-    User_Repository_Test.cases;
   ]
