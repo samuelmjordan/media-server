32dfd29b163d1cd031b00dd0ea78f43bfca66460
jordan12sam <jordan12sam@gmail.com>
2025-07-30 10:34:40 +0100

    formatting

--- lib/handler/File_Handler.ml
+++ lib/handler/File_Handler.ml
@@ -4,10 +4,10 @@ open Handler
 let get_directory req =
   with_query_param req "path" (fun path ->
   with_query_param req "mime" (fun mime_filter ->
-    let* result = File_Service.get_directory_files ~path ~mime_filter () in
-    match result with
-      | Error e -> Dream.respond ~status:`Internal_Server_Error e
-      | Ok files ->
+  let* result = File_Service.get_directory_files ~path ~mime_filter () in
+  match result with
+  | Error e -> Dream.respond ~status:`Internal_Server_Error e
+  | Ok files ->
     let json = `List (List.map File.file_to_json files) in
     Dream.json (Yojson.Safe.to_string json)))
 
@@ -15,26 +15,26 @@ let delete_directory req =
   with_query_param req "path" (fun path ->
   let* result = File_Service.delete_directory path in
   match result with
-    | Error _ -> Dream.respond ~status:`Internal_Server_Error "internal server error"
-    | Ok 0 -> Dream.respond ~status:`Not_Found ({|{"deleted": 0}|})
-    | Ok count -> Dream.json ~status:`OK (Printf.sprintf {|{"deleted": %d}|} count))
+  | Error _ -> Dream.respond ~status:`Internal_Server_Error "internal server error"
+  | Ok 0 -> Dream.respond ~status:`Not_Found ({|{"deleted": 0}|})
+  | Ok count -> Dream.json ~status:`OK (Printf.sprintf {|{"deleted": %d}|} count))
     
 let scan_directory req =
   with_query_param req "path" (fun path ->
     let* files = File_Service.scan_directory path in
     match files with
-      | Error _ -> Dream.respond ~status:`Internal_Server_Error "internal server error"
-      | Ok files -> 
-    let json = `List (List.map File.file_to_json files) in
-    Dream.json (Yojson.Safe.to_string json))
+    | Error _ -> Dream.respond ~status:`Internal_Server_Error "internal server error"
+    | Ok files -> 
+      let json = `List (List.map File.file_to_json files) in
+      Dream.json (Yojson.Safe.to_string json))
 
 let get_file req =
   let file_id_str = Dream.param req "file_id" in
   match File.File_Uuid.from_string file_id_str with
-    | Error _ -> Dream.respond ~status:`Bad_Request "invalid file id format"
-    | Ok file_id ->
-  let* result = File_Service.get_file file_id in
-  match result with
+  | Error _ -> Dream.respond ~status:`Bad_Request "invalid file id format"
+  | Ok file_id ->
+    let* result = File_Service.get_file file_id in
+    match result with
     | Error _ -> Dream.respond ~status:`Internal_Server_Error "internal server error"
     | Ok None -> Dream.respond ~status:`Not_Found "file not found"
     | Ok (Some file) -> Dream.json (Yojson.Safe.to_string (File.file_to_json file))
